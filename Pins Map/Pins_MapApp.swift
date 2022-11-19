4e77cbff2c4bf101d3b09ed1a47abe55d19c4d18
Jonah Evangelista <jonahevangelista@yahoo.com>
2022-11-18 23:20:20 -0700

    Initial Commit

---
+++ Pins Map/Pins_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pins_MapApp.swift
+//  Pins Map
+//
+//  Created by Jonah Marie Evangelista on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Pins_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
